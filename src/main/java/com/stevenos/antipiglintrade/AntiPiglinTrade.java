b00a0f23398556c849c65c4c3a6837ae0c89cef8
Steven-OS <StevenWintery@gmail.com>
2024-05-02 21:10:54 +0200

    - Optimization for the world-checking. - Prefix color change.

--- src/main/java/com/stevenos/antipiglintrade/AntiPiglinTrade.java
+++ src/main/java/com/stevenos/antipiglintrade/AntiPiglinTrade.java
@@ -3,7 +3,6 @@ package com.steven_os.antipiglintrade;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Piglin;
 import org.bukkit.entity.Player;
@@ -14,29 +13,27 @@ import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.List;
+
 public class AntiPiglinTrade extends JavaPlugin implements Listener {
 
-    private FileConfiguration config;
     private String prefix;
     private String errorMessage;
+    private List<String> disallowedWorlds;
 
     @Override
     public void onEnable() {
+        registerEventsAndCommands();
+        loadConfig();
+        initMetrics();
+        printConsoleMessage();
+    }
+
+    private void registerEventsAndCommands() {
         getServer().getPluginManager().registerEvents(this, this);
         com.steven_os.antipiglintrade.AntiPiglinTradeCommandExecutor executor = new com.steven_os.antipiglintrade.AntiPiglinTradeCommandExecutor(this);
         getCommand("antipiglintrade").setExecutor(executor);
         getCommand("antipiglintrade").setTabCompleter(executor);
-        loadConfig();
-        printConsoleMessage();
-        boolean isEnableBStats = getConfig().getBoolean("enable-bstats", true); // The second parameter is default value
-
-        // Initialize Metrics only if 'enable-bstats' is true
-        if (isEnableBStats) {
-            int pluginId = 21750; // Replace this with your bStats plugin ID!
-            com.steven_os.antipiglintrade.Metrics metrics = new com.steven_os.antipiglintrade.Metrics(this, pluginId);
-            // Add your custom charts or other bStats-related logic here
-            // only if 'enable-bstats' is true.
-        }
     }
 
     @Override
@@ -46,47 +43,52 @@ public class AntiPiglinTrade extends JavaPlugin implements Listener {
 
     public void loadConfig() {
         saveDefaultConfig();
-        config = getConfig();
-        prefix = ChatColor.translateAlternateColorCodes('&', config.getString("prefix", "&eAntiPiglinTrade &8»&f "));
-        errorMessage = ChatColor.translateAlternateColorCodes('&', config.getString("error-message", "&cPiglin bartering is disabled."));
+        reloadConfig();
+        prefix = ChatColor.translateAlternateColorCodes('&', getConfig().getString("prefix", "&6AntiPiglinTrade &8»&f "));
+        errorMessage = ChatColor.translateAlternateColorCodes('&', getConfig().getString("error-message", "&cPiglin bartering is disabled."));
+        disallowedWorlds = getConfig().getStringList("world-list");
     }
 
     @EventHandler
     public void onEntityPickupItem(EntityPickupItemEvent event) {
-        if(event.getEntityType() == EntityType.PIGLIN) {
-            ItemStack pickedItem = event.getItem().getItemStack();
-            if(pickedItem.getType() == Material.GOLD_INGOT && !isAllowedWorld(event.getEntity().getWorld().getName())) {
-                event.setCancelled(true);
-            }
+        if (event.getEntityType() == EntityType.PIGLIN && isGoldIngot(event.getItem().getItemStack()) && isDisallowedWorld(event.getEntity().getWorld().getName())) {
+            event.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onClickEvent(PlayerInteractEntityEvent event) {
-        if (!(event.getRightClicked() instanceof Piglin)) {
-            return;
-        }
-
-        Player player = event.getPlayer();
-        ItemStack item = player.getInventory().getItemInMainHand();
-        if (item.getType() == Material.GOLD_INGOT && !isAllowedWorld(player.getWorld().getName())) {
-            player.sendMessage(getPrefix() + errorMessage);
-            event.setCancelled(true);
+        if (event.getRightClicked() instanceof Piglin) {
+            Player player = event.getPlayer();
+            if (isGoldIngot(player.getInventory().getItemInMainHand()) && isDisallowedWorld(player.getWorld().getName())) {
+                player.sendMessage(prefix + errorMessage);
+                event.setCancelled(true);
+            }
         }
     }
 
-    private boolean isAllowedWorld(String worldName) {
-        return !config.getStringList("world-list").contains(worldName);
+    private boolean isGoldIngot(ItemStack item) {
+        return item.getType() == Material.GOLD_INGOT;
+    }
+
+    private boolean isDisallowedWorld(String worldName) {
+        return disallowedWorlds.contains(worldName);
+    }
+
+    private void initMetrics() {
+        boolean isEnableBStats = getConfig().getBoolean("enable-bstats", true);
+        if (isEnableBStats) {
+            int pluginId = 21750;
+            new Metrics(this, pluginId);
+        }
     }
 
     private void printConsoleMessage() {
         getLogger().info("--------------------------------------------------------------");
-        getLogger().info("");
         getLogger().info("» AntiPiglinTrade has loaded successfully!");
         getLogger().info("» Made by: Steven-OS");
         getLogger().info("» Plugin version: " + getDescription().getVersion());
         getLogger().info("» Server version: " + Bukkit.getServer().getVersion());
-        getLogger().info("");
         getLogger().info("--------------------------------------------------------------");
     }
 
